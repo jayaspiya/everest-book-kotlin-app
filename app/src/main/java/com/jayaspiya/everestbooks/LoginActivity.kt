6c0fabe68bf4a741c403ac8b4a4baf7353f5cf64
Jayas Piya <pzayazz@gmail.com>
2021-06-13 18:13:13 +0545

    Activity Switch on Button Click

--- app/src/main/java/com/jayaspiya/everestbooks/LoginActivity.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.jayaspiya.everestbooks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var btnSignup: Button
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnSignup = findViewById(R.id.btnSignup)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+        btnLogin.setOnClickListener { }
     }
 }
--- app/src/main/java/com/jayaspiya/everestbooks/SignupActivity.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/SignupActivity.kt
@@ -1,11 +1,21 @@
 package com.jayaspiya.everestbooks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var btnSignup:Button
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        btnSignup = findViewById(R.id.btnSignup)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignup.setOnClickListener {  }
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent) }
     }
 }
