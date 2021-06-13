503d8bd31362a19c85c6d6f392b51d1187e0d8a7
Jayas Piya <pzayazz@gmail.com>
2021-06-13 18:23:03 +0545

    Form Validation

--- app/src/main/java/com/jayaspiya/everestbooks/LoginActivity.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/LoginActivity.kt
@@ -3,20 +3,41 @@ package com.jayaspiya.everestbooks
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var btnSignup: Button
     private lateinit var btnLogin: Button
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         btnSignup = findViewById(R.id.btnSignup)
         btnLogin = findViewById(R.id.btnLogin)
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+
+
         btnSignup.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
-        btnLogin.setOnClickListener { }
+        btnLogin.setOnClickListener {
+            if (TextUtils.isEmpty(etUsername.text)) {
+                etUsername.requestFocus()
+                etUsername.error = "Username can not be empty."
+            } else if (TextUtils.isEmpty(etPassword.text)) {
+                etPassword.requestFocus()
+                etPassword.error = "Username can not be empty."
+            }
+            else{
+                Toast.makeText(this@LoginActivity, "Login: ${etUsername.text}", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/jayaspiya/everestbooks/SignupActivity.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/SignupActivity.kt
@@ -3,17 +3,37 @@ package com.jayaspiya.everestbooks
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var btnSignup:Button
     private lateinit var btnLogin: Button
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         btnSignup = findViewById(R.id.btnSignup)
         btnLogin = findViewById(R.id.btnLogin)
-        btnSignup.setOnClickListener {  }
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+
+        btnSignup.setOnClickListener {
+            if (TextUtils.isEmpty(etUsername.text)) {
+                etUsername.requestFocus()
+                etUsername.error = "Username can not be empty."
+            } else if (TextUtils.isEmpty(etPassword.text)) {
+                etPassword.requestFocus()
+                etPassword.error = "Username can not be empty."
+            }
+            else{
+                Toast.makeText(this@SignupActivity, "Signup: ${etUsername.text}", Toast.LENGTH_LONG).show()
+            }
+        }
         btnLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent) }
