3986012d86184f631c467e340fb510592fb5d2c6
Jayas Piya <pzayazz@gmail.com>
2021-06-26 19:45:17 +0545

    Resolved layout-v21 code dublication

--- app/src/main/java/com/jayaspiya/everestbooks/BookActivity.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/BookActivity.kt
@@ -15,7 +15,6 @@ class BookActivity : AppCompatActivity() {
     private lateinit var tvDescription: TextView
     private lateinit var ivBook: ImageView
     private lateinit var btnAddToCart: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book)
@@ -27,14 +26,6 @@ class BookActivity : AppCompatActivity() {
         btnAddToCart = findViewById(R.id.btnAddToCart)
 
         val book: Book? = intent.getParcelableExtra("book")
-
-//        val bookMap = mapOf(
-//            "title" to "A Man with One of Those Faces",
-//            "price" to 450,
-//            "author" to "Caimh Mcdonnell",
-//            "description" to "The first time somebody tried to kill him was an accident. The second time was deliberate. Now Paul Mulchrone finds himself on the run with nobody to turn to except a nurse who has read one-too-many crime novels and a renegade copper with a penchant for violence. Together they must solve one of the most notorious crimes in Irish history or else they’ll be history.",
-//            "imageUrl" to "https://silly-visvesvaraya-f21a81.netlify.app/assets/a-man-with-one-of-those-faces--front.png"
-//        )
 
         if (book != null) {
             tvTitle.text = book.title.toString()
