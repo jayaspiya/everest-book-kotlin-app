6b55ff97f32c20e9bb24cd7cfb51385ba9fa5570
Jayas Piya <pzayazz@gmail.com>
2021-08-05 20:33:56 +0545

    Room Persistence POJO error

--- app/src/main/java/com/jayaspiya/everestbooks/database/EverestDB.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/database/EverestDB.kt
@@ -2,6 +2,8 @@ package com.jayaspiya.everestbooks.database
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.jayaspiya.everestbooks.dao.UserDAO
 import com.jayaspiya.everestbooks.entity.User
 
