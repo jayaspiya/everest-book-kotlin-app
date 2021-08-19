d281bd04a013a3c0d6eb43210070dd9ce9fbbd94
Jayas Piya <pzayazz@gmail.com>
2021-08-19 06:58:26 +0545

    :sparkles: Added User Model

---
+++ app/src/main/java/com/jayaspiya/everestbooks/model/User.kt
@@ -0,0 +1,13 @@
+package com.jayaspiya.everestbooks.model
+
+data class User(
+    val _id: String? = "",
+    val email: String? = null,
+    val password: String? = null,
+    val firstname: String? = null,
+    val lastname: String? = null,
+    val address: String? = null,
+    val phone: String? = null,
+    val profile: String? = null,
+    val gender: String? = null,
+)
