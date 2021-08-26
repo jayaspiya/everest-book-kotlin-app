cf5e11597a3c1298fb5be354ed9b1047214ea606
Jayas Piya <pzayazz@gmail.com>
2021-08-26 19:50:55 +0545

    :lipstick: Book Design

--- app/src/main/java/com/jayaspiya/everestbooks/fragments/HomeFragment.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jayaspiya.everestbooks.R
@@ -38,6 +39,7 @@ class HomeFragment : Fragment() {
                         bookList = response.data!!
                         val adapter = BookAdapter(bookList, requireContext())
                         bookRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+//                        bookRecyclerView.layoutManager = GridLayoutManager(requireContext(),2)
                         bookRecyclerView.adapter = adapter
                         return@withContext view
                     }
--- app/src/main/java/com/jayaspiya/everestbooks/fragments/LibraryFragment.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/fragments/LibraryFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
 import com.jayaspiya.everestbooks.CartActivity
 import com.jayaspiya.everestbooks.R
 
