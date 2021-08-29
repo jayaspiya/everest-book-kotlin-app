00bdd128c9d323f496003d8672a836e2f875392f
Jayas Piya <pzayazz@gmail.com>
2021-08-29 09:10:55 +0545

    :sparkles: User Register Full Details

--- app/src/main/java/com/jayaspiya/everestbooks/SignupActivity.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/SignupActivity.kt
@@ -19,6 +19,10 @@ import kotlinx.coroutines.withContext
 class SignupActivity : AppCompatActivity() {
     private lateinit var btnSignup: Button
     private lateinit var tvLogin: TextView
+    private lateinit var etFirstname: EditText
+    private lateinit var etLastname: EditText
+    private lateinit var etAddress: EditText
+    private lateinit var etPhone: EditText
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var etCPassword: EditText
@@ -28,60 +32,78 @@ class SignupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
         btnSignup = findViewById(R.id.btnSignup)
         tvLogin = findViewById(R.id.tvLogin)
+        etFirstname = findViewById(R.id.etFirstname)
+        etLastname = findViewById(R.id.etLastname)
+        etAddress = findViewById(R.id.etAddress)
+        etPhone = findViewById(R.id.etPhone)
         etEmail = findViewById(R.id.etEmail)
         etPassword = findViewById(R.id.etPassword)
         etCPassword = findViewById(R.id.etCPassword)
 
         btnSignup.setOnClickListener {
-            if (TextUtils.isEmpty(etEmail.text)) {
-                etEmail.requestFocus()
-                etEmail.error = "Email can not be empty."
-            } else if (TextUtils.isEmpty(etPassword.text)) {
-                etPassword.requestFocus()
-                etPassword.error = "Password can not be empty."
-            } else if (TextUtils.isEmpty(etCPassword.text)) {
-                etCPassword.requestFocus()
-                etCPassword.error = "Confirm Password can not be empty."
-            } else if (etPassword.text.toString() != etCPassword.text.toString()) {
-                etCPassword.requestFocus()
-                etCPassword.error = "Password and Confirm Password does not match"
-            } else {
-                val user =
-                    User(email = etEmail.text.toString(), password = etPassword.text.toString())
-                // Room DB
-//                CoroutineScope(Dispatchers.IO).launch {
-//                    EverestDB.getInstance(this@SignupActivity)
-//                        .getUserDAO().registerUser(user)
-//                    withContext(Dispatchers.Main){
-//                        Toast.makeText(this@SignupActivity, "User created", Toast.LENGTH_LONG).show()
-//                        startActivity(Intent(this@SignupActivity, LoginActivity::class.java))
-//                        finish()
-//                    }
-//                }
-
-                // Retrofit
-                try {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        val userRepo = UserRepository()
-                        val response = userRepo.registerUser(user)
-                        if (response.success == true) {
-                            withContext(Main) {
-                                Toast.makeText(
-                                    this@SignupActivity,
-                                    response.message,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                startActivity(
-                                    Intent(
+            when {
+                TextUtils.isEmpty(etFirstname.text) -> {
+                    etFirstname.requestFocus()
+                    etFirstname.error = "First name can not be empty."
+                }
+                TextUtils.isEmpty(etLastname.text) -> {
+                    etLastname.requestFocus()
+                    etLastname.error = "Last name can not be empty."
+                }
+                TextUtils.isEmpty(etAddress.text) -> {
+                    etAddress.requestFocus()
+                    etAddress.error = "Address can not be empty."
+                }
+                TextUtils.isEmpty(etPhone.text) -> {
+                    etPhone.requestFocus()
+                    etPhone.error = "Phone Number can not be empty."
+                }
+                TextUtils.isEmpty(etEmail.text) -> {
+                    etEmail.requestFocus()
+                    etEmail.error = "Email can not be empty."
+                }
+                TextUtils.isEmpty(etPassword.text) -> {
+                    etPassword.requestFocus()
+                    etPassword.error = "Password can not be empty."
+                }
+                TextUtils.isEmpty(etCPassword.text) -> {
+                    etCPassword.requestFocus()
+                    etCPassword.error = "Confirm Password can not be empty."
+                }
+                etPassword.text.toString() != etCPassword.text.toString() -> {
+                    etCPassword.requestFocus()
+                    etCPassword.error = "Password and Confirm Password does not match"
+                }
+                else -> {
+                    val user =
+                        User(email = etEmail.text.toString(), password = etPassword.text.toString(),
+                            firstname = etFirstname.text.toString(), lastname = etLastname.text.toString(),
+                            address = etAddress.text.toString(), phone = etPhone.text.toString()
+                        )
+                    // Retrofit
+                    try {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val userRepo = UserRepository()
+                            val response = userRepo.registerUser(user)
+                            if (response.success == true) {
+                                withContext(Main) {
+                                    Toast.makeText(
                                         this@SignupActivity,
-                                        LoginActivity::class.java
+                                        response.message,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    startActivity(
+                                        Intent(
+                                            this@SignupActivity,
+                                            LoginActivity::class.java
+                                        )
                                     )
-                                )
+                                }
                             }
                         }
+                    } catch (ex: Exception) {
+                        println(ex)
                     }
-                } catch (ex: Exception) {
-                    println(ex)
                 }
             }
         }
