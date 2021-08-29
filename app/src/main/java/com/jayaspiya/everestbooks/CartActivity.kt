adb8d4003e5720ecc65a34f1f76aa23bb7a0bff4
Jayas Piya <pzayazz@gmail.com>
2021-08-29 10:18:40 +0545

    :sparkles: Implementation Of ProgressBar

--- app/src/main/java/com/jayaspiya/everestbooks/CartActivity.kt
+++ app/src/main/java/com/jayaspiya/everestbooks/CartActivity.kt
@@ -1,8 +1,14 @@
 package com.jayaspiya.everestbooks
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jayaspiya.everestbooks.adapter.BookAdapter
@@ -16,10 +22,13 @@ import kotlinx.coroutines.withContext
 
 class CartActivity : AppCompatActivity() {
     private lateinit var bookRecyclerView: RecyclerView
+    private lateinit var progressBar: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
         bookRecyclerView = findViewById(R.id.bookRecyclerView)
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
         try {
             CoroutineScope(IO).launch {
                 val userRepository = UserRepository()
@@ -27,6 +36,7 @@ class CartActivity : AppCompatActivity() {
                 if(response.success == true){
                     val bookList = response.data!!
                     withContext(Main){
+                        progressBar.visibility = View.GONE
                         Toast.makeText(this@CartActivity, response.message, Toast.LENGTH_SHORT).show()
                         val adapter = CartAdapter(bookList, this@CartActivity)
                         bookRecyclerView.layoutManager = LinearLayoutManager(
