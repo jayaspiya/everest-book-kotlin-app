e1369b11c484489f3ea79f9a92ed5f0752f81aff
Jayas Piya <pzayazz@gmail.com>
2021-09-22 20:45:27 +0545

    :heavy_plus_sign: Added Retrofit Dependencies

--- everestbook_wearos/src/main/java/com/jayaspiya/everestbook_wearos/LoginActivity.kt
+++ everestbook_wearos/src/main/java/com/jayaspiya/everestbook_wearos/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.jayaspiya.everestbook_wearos
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.jayaspiya.everestbook_wearos.databinding.ActivityLoginBinding
 
 class LoginActivity : Activity() {
@@ -10,9 +14,30 @@ class LoginActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            when {
+                TextUtils.isEmpty(binding.etEmail.text) -> {
+                    binding.apply {
+                        etEmail.requestFocus()
+                        etEmail.error = "Email can not be empty."
+                    }
+                }
+                TextUtils.isEmpty(binding.etPassword.text) -> {
+                    binding.apply {
+                        etPassword.requestFocus()
+                        etPassword.error = "Password can not be empty."
+                    }
+                }
+                else -> {
+                    login()
+                }
+            }
+
+        }
+    }
+    private fun login() {
 
     }
 }
